c861487ce74c29f8bf625657f6b0aa02601ef6c9
Patrick Falk Nielsen <patrick.nielsen@bestseller.com>
2020-06-24 11:41:04 +0200

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+#Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#Minimal dependencies
+brew install git
+brew install ansible
+
+#Git clone the repo
+git clone https://github.com/patrickfnielsen/osx-dev
+
+#Fix ansible localhost warning
+export ANSIBLE_LOCALHOST_WARNING=False
+
+if [ -z $(git config --global user.email) ]
+then
+  echo Enter your e-mail:
+  read gitEmail
+
+  echo Enter your name:
+  read gitName
+fi
+
+#Run ansible playbook
+ansible-playbook setup-playbook.yml --extra-vars "git_global_name=$gitName git_global_email=$gitEmail"
